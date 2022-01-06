6ad6243fa72e427fb3a45ffb37d43efac070c8e9
Michael Deyaso <mdeyaso@fusioniq.io>
2022-01-06 08:46:23 +0300

    Get endpoint for categories now returns nested data

--- controllers/categories/index.js
+++ controllers/categories/index.js
@@ -1,14 +1,20 @@
 const Category = require('../../models').Category;
 const Product = require('../../models').Product;
+const ProductType = require('../../models').ProductType;
 
 
 module.exports = {
     getAllCategories: async(req, res, next)=>{
         return Category.findAll({
-            include: [{
-                model: Product,
-                as: "products"
-            }]
+            // include: [{
+            //     model: ProductType,
+            //     // as: "Products"
+            // }]
+            include: {
+                all: true,
+                nested: true
+            }
+            // include: ProductType
         })
         .then(result => res.status(200).send(result))
         .catch(error => res.status(400).send(error));
